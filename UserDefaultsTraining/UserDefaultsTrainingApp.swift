ae922798fd0d4661308718f10d0c5198e3a7fa05
DogukanAkyuz <sdogukanakyuz@gmail.com>
2024-12-02 19:51:42 +0300

    Initial Commit

---
+++ UserDefaultsTraining/UserDefaultsTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsTrainingApp.swift
+//  UserDefaultsTraining
+//
+//  Created by Doğukan Akyüz on 2.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
